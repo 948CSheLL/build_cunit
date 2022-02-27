06fbd2a01b17906b0bd4074b599da4c37109b544
TaoXinCan <2418942810@qq.com>
2022-02-26 18:28:54 -0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 function exec_command() {
     ${1}
     exit_status=$(($?))
-    if [ ${exit_status} -ne 0 ];then
+    if [ ${exit_status} -ne 0 ] && [ ${exit_status} -ne 1 ];then
         exit ${exit_status}
     fi
 }
@@ -13,11 +13,14 @@ exec_command "apt-get install autoconf"
 exec_command "apt-get install libtool"
 exec_command "cd CUnit*"
 exec_command "mv configure.in configure.ac"
-exec_command "aclocal"
-exec_command "autoheader"
-exec_command "libtoolize --automake --copy --debug --force"
-exec_command "automake --add-missing"
-exec_command "autoconf"
+for((i=1; i<=2; i=i+1))
+do
+    exec_command "aclocal"
+    exec_command "autoheader"
+    exec_command "libtoolize --automake --copy --debug --force"
+    exec_command "automake --add-missing"
+    exec_command "autoconf"
+done
 if [ -d "/usr/include/unittest" ];then
     exec_command "rm -rf /usr/include/unittest"
 fi
